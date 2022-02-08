f480cfe9746dc7d236c2633fca6981676e1ca844
Olatoye Daramola <daramolatoye@gmail.com>
2022-02-08 14:53:21 +0100

    ...

--- first/src/main/kotlin/Arrays/ArrayFunc.kt
+++ first/src/main/kotlin/Arrays/ArrayFunc.kt
@@ -2,19 +2,37 @@ fun <T> sizeOf(array: Array<T>): Int {
     return array.size
 }
 
+fun areAnagrams(firstWord: String, secondWord: String): Boolean {
+    if (firstWord.length != secondWord.length) return false
+    for (i in firstWord) {
+        if (!(i in secondWord)) return false
+    }
+    return true
+}
+
+fun isPangram(word: String): Boolean {
+    var alphabets = "abcdefghijklmnopqrstuvxyz"
+    
+    for (i in alphabets) {
+        if (!(i in word)) return false
+    }
+    return true
+}
+
 fun main() {
     // println(sizeOf(arrayOf("A", "B", "C")))
 
-    var mutableList: MutableList<String> = mutableListOf("Apple", "Orange")
-    mutableList.add("Banana")
-    mutableList.add("Mango")
+    // var mutableList: MutableList<String> = mutableListOf("Apple", "Orange")
+    // mutableList.add("Banana")
+    // mutableList.add("Mango")
 
-    for (element in mutableList) {
-        println(element)
-    }
+    // for (element in mutableList) {
+    //     println(element)
+    // }
 
-    println("\n" + mutableList.removeAt(1))
+    // println("\n" + mutableList.removeAt(1))
 
-    val newList = mutableList.removeAt(1)
-    println(newList)
+    // val newList = mutableList.removeAt(1)
+    // println(newList)
+    println(isPangram("abcdefghijklmnopqruvxyz"))
 }
